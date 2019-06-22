83c07646bf8c9b8a4e790568b74f77aec99846cf
ducdmn <ndminhduc@gmail.com>
2019-06-22 10:32:46 +0700

    add backend

---
+++ Angular7CRUDDemo.Backend/Angular7CRUDDemo.Backend/CorsMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular7CRUDDemo.Backend
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class CorsMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public CorsMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+            httpContext.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+            httpContext.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Accept");
+            httpContext.Response.Headers.Add("Access-Control-Allow-Methods", "POST,GET,PUT,PATCH,DELETE,OPTIONS");
+            return this.next(httpContext);
+        }
+    }
+
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CorsMiddleware>();
+        }
+    }
+}
